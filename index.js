a862e8647e81e58c721998452f502524edd2d5d8
R-kjim <rkayjim@gmail.com>
2024-07-03 17:12:11 +0300

    an array as a function parameter

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array){
+    for(let row of array){
+    let result=array.find(arrayPlace=>arrayPlace['result']==='W')
+    if(result){
+        let i=array.indexOf(result)
+        return array[i].year
+    }
+}}
